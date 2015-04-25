3b488f735334b4bc9d661abf0f2fdb58240d13f4
Sohil Veljee <vsohail09@gmail.com>
2015-04-24 18:11:49 -0700

    Sohil | Added more ingredient effects

---
+++ KitchenNinja.spritebuilder/Onion.h
@@ -0,0 +1,13 @@
+//
+//  Onion.h
+//  KitchenNinja
+//
+//  Created by Sohil on 4/24/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Ingredient.h"
+
+@interface Onion : Ingredient
+
+@end
---
+++ KitchenNinja.spritebuilder/Onion.m
@@ -0,0 +1,13 @@
+//
+//  Onion.m
+//  KitchenNinja
+//
+//  Created by Sohil on 4/24/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Onion.h"
+
+@implementation Onion
+
+@end
